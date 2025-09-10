b89167d0e2c1db2ceb82acd6869cbf2031d540cc
Obatola Opeyemi <opeyemiobatola@gmail.com>
2025-09-10 01:49:43 +0100

    Chore (Features): fix error imagess in features section

--- src/App.jsx
+++ src/App.jsx
@@ -20,17 +20,17 @@ function App() {
             description="The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere."
           />
           <Features
-            image="/public/images/icon-security.svg"
+            image="/images/icon-security.svg"
             title="Security you can trust"
             description="2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files."
           />
           <Features
-            image="/public/images/icon-collaboration.svg"
+            image="/images/icon-collaboration.svg"
             title="Real-time collaboration"
             description="Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required."
           />
           <Features
-            image="/public/images/icon-any-file.svg"
+            image="/images/icon-any-file.svg"
             title="Store any type of file"
             description="Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared."
           />
